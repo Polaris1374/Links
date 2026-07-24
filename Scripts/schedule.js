d252d0905a5cf34c5bae6466852e5f47a7449af9
Kill3rKai <89795034+Kill3rKai@users.noreply.github.com>
2026-07-24 20:10:04 +0100

    changed stream time

--- Scripts/schedule.js
+++ Scripts/schedule.js
@@ -20,7 +20,7 @@ const scheduleData = [
     {
         game: "Shift At Midnight",
         date: "2026-07-24",
-        time: "21:30",
+        time: "22:00",
         timezone: "SAST",
         duration: 200,
         note: "Co-op with Kill3rKai",
