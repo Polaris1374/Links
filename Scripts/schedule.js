9c15833af769476a80f5195978efb237c4be33e2
Kill3rKai <89795034+Kill3rKai@users.noreply.github.com>
2026-07-25 22:00:42 +0100

    chnaged stream

--- Scripts/schedule.js
+++ Scripts/schedule.js
@@ -20,7 +20,7 @@ const scheduleData = [
     {
         game: "Scrap Mechanic 1.0",
         date: "2026-07-25",
-        time: "23:00",
+        time: "23:30",
         timezone: "SAST",
         duration: 180,
         note: "Co-op with Kill3rKai",
