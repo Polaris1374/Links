cf8adb1ea4629186522dd830cf72c33e8135a7b2
Kill3rKai <89795034+Kill3rKai@users.noreply.github.com>
2026-08-22 21:15:43 +0100

    updated schedule

--- Scripts/schedule.js
+++ Scripts/schedule.js
@@ -18,9 +18,9 @@
 
 const scheduleData = [
     {
-        game: "TBD",
+        game: "How To Fish",
         date: "2026-08-22",
-        time: "20:00",
+        time: "00:00",
         timezone: "IST",
         duration: 120,
         note: "TBD",
